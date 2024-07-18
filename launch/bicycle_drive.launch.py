7e80cb02e785baecec52cf7bc1c70636c7605cf3
Ashraf AlAssi <smart.teen.12345@gmail.com>
2024-07-18 22:38:17 +0300

    Nothing much

--- launch/bicycle_drive.launch.py
+++ launch/bicycle_drive.launch.py
@@ -4,12 +4,14 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import (
+    DeclareLaunchArgument,
     ExecuteProcess,
     IncludeLaunchDescription,
     RegisterEventHandler,
 )
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PythonExpression
 
 from launch_ros.actions import Node
 
@@ -21,6 +23,35 @@ pkg_share_directory = get_package_share_directory(pkg_name)
 
 def generate_launch_description():
 
+    pkg_path = os.path.join(pkg_share_directory)
+    xacro_file = os.path.join(pkg_path, "urdf", "bicycle_drive.xacro.urdf")
+    robot_description_raw = xacro.process_file(xacro_file).toxml()
+    params = {"robot_description": robot_description_raw, "use_sim_time": True}
+
+    node_robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[params],
+    )
+
+    node_joint_state_publisher_gui = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        name="joint_state_publisher_gui",
+        arguments=[xacro_file],
+    )
+
+    rviz_config = os.path.join(pkg_path, "config", "config.rviz")
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", rviz_config],
+        output="screen",
+    )
+
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [
@@ -31,34 +62,6 @@ def generate_launch_description():
                 ),
             ]
         ),
-    )
-
-    pkg_path = os.path.join(pkg_share_directory)
-    xacro_file = os.path.join(pkg_path, "urdf", "bicycle_drive.xacro.urdf")
-
-    # Method 1
-    # doc = xacro.parse(open(xacro_file))
-    # xacro.process_doc(doc)
-    # robot_description_raw = doc.toxml()
-
-    # Method 2
-    robot_description_raw = xacro.process_file(xacro_file).toxml()
-
-    params = {"robot_description": robot_description_raw}
-
-    node_robot_state_publisher = Node(
-        name="robot_state_publisher",
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="screen",
-        parameters=[params],
-    )
-
-    node_joint_state_publisher_gui = Node(
-        package="joint_state_publisher_gui",
-        executable="joint_state_publisher_gui",
-        name="joint_state_publisher_gui",
-        arguments=[xacro_file],
     )
 
     spawn_entity = Node(
@@ -92,20 +95,10 @@ def generate_launch_description():
         output="screen",
     )
 
-    rviz = Node(
-        package="rviz2",
-        executable="rviz2",
-        arguments=[
-            "-d",
-            os.path.join(pkg_path, "config", "config.rviz"),
-        ],
-        output="screen",
-    )
-
     return LaunchDescription(
         [
             rviz,
-            # node_joint_state_publisher_gui,
+            node_joint_state_publisher_gui,
             node_robot_state_publisher,
             gazebo,
             spawn_entity,
